0de47addf18a491800e8f35117e7843d578aae5b
misterchrishoffman@gmail.com <misterchrishoffman@gmail.com>
2025-08-05 20:04:01 +0000

    Changed Bigquery adapter to - PARSE_DATE('%Y-%m-%d', 'YYYY-MM-DD')

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -2,8 +2,8 @@
 --for BQ adapters use "DATE('01/01/2000','mm/dd/yyyy')"
 with days as (
     {{dbt_utils.date_spine('day'
-    , "to_date('01/01/2000','mm/dd/yyyy')"
-    , "to_date('01/01/2027','mm/dd/yyyy')"
+    , "PARSE_DATE('%Y-%m-%d', 'YYYY-MM-DD')"
+    , "PARSE_DATE('%Y-%m-%d', 'YYYY-MM-DD')"
     )
     }}
 ),
