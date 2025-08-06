56149422bdf6b68bf68edfdeff55f20c462195db
Chris Hoffman <misterchrishoffman@gmail.com>
2025-08-06 13:26:53 +0000

    Adding in version info. Started adding groups and owners.

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,47 @@
+with 
+
+orders as (
+    
+    select * from {{ ref('stg_orders')}}
+
+),
+
+order_items as (
+    
+    select * from {{ ref('order_items')}}
+
+),
+
+order_items_summary as (
+
+    select
+
+        order_items.order_id,
+
+        sum(supply_cost) as order_cost,
+        sum(is_food_item) as count_food_items,
+        sum(is_drink_item) as count_drink_items
+
+
+    from order_items
+
+    group by 1
+
+),
+
+
+compute_booleans as (
+    select
+
+        orders.*,
+        count_food_items > 0 as is_food_order,
+        count_drink_items > 0 as is_drink_order,
+        order_cost as total_order_cost,
+        cast(ordered_at as date) as location_ordered_at
+
+    from orders
+    
+    left join order_items_summary on orders.order_id = order_items_summary.order_id
+)
+
+select * from compute_booleans
